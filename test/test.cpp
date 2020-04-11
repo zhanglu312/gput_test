60b29a47aec7a40aafc53adbec155912c176f235
zhanglu <lu_zhang0928@163.com>
2020-04-11 11:17:35 +0800

    gputest

---
+++ test/test.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+ 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/ocl.hpp>
+#include <opencv2/core/cuda.hpp>
+#include <opencv2/cudaobjdetect.hpp>
+#include <opencv2/cudaimgproc.hpp>
+ 
+ 
+int main(int argc, char**argv) {
+    std::cout << "OpenCV version=" << std::hex << CV_VERSION << std::dec << std::endl;
+ 
+    cv::Mat frame, gray;
+    cv::UMat uframe, uFrameGray;
+    cv::cuda::GpuMat image_gpu, image_gpu_gray;
+    cv::VideoCapture capture("test.mp4");
+
+    bool useNone = (argc == 1);
+    std::cout << "Use None=" << useNone << std::endl;
+
+    bool useOpenCL = (argc == 2);
+    std::cout << "Use OpenCL=" << useOpenCL << std::endl;
+    cv::ocl::setUseOpenCL(useOpenCL);
+ 
+    bool useCuda = (argc == 3);
+    std::cout << "Use CUDA=" << useCuda << std::endl;
+ 
+    cv::Ptr<cv::CascadeClassifier> cascade = cv::makePtr<cv::CascadeClassifier>("/home/psdz/opencv-3.4.0/data/lbpcascades/lbpcascade_frontalface_improved.xml");
+    cv::Ptr<cv::cuda::CascadeClassifier> cascade_gpu = cv::cuda::CascadeClassifier::create("/home/psdz/opencv-3.4.0/data/lbpcascades/lbpcascade_frontalface_improved.xml");
+ 
+    double time = 0.0;
+    int nb = 0;
+    if(capture.isOpened()) {
+        for(;;) {
+            capture >> frame;
+            if(frame.empty() || nb >= 1000) {
+                break;
+            }
+ 
+            std::vector<cv::Rect> faces;
+            double t = 0.0;
+            if(useOpenCL) {
+                t = (double) cv::getTickCount();
+                frame.copyTo(uframe);
+                cv::cvtColor(uframe, uFrameGray, CV_BGR2GRAY);
+                cascade->detectMultiScale(uFrameGray, faces);
+                t = ((double) cv::getTickCount() - t) / cv::getTickFrequency();
+            }
+
+	    if (useCuda) 
+	    {
+                t = (double) cv::getTickCount();
+                image_gpu.upload(frame);
+                cv::cuda::cvtColor(image_gpu, image_gpu_gray, CV_BGR2GRAY);
+                cv::cuda::GpuMat objbuf;
+                cascade_gpu->detectMultiScale(image_gpu_gray, objbuf);
+                cascade_gpu->convert(objbuf, faces);
+                t = ((double) cv::getTickCount() - t) / cv::getTickFrequency();
+            }
+ 
+	    if (useNone) 
+            {
+		t = (double) cv::getTickCount();
+		cv::cvtColor(frame, gray, CV_BGR2GRAY);
+                cascade->detectMultiScale(gray, faces);
+                t = ((double) cv::getTickCount() - t) / cv::getTickFrequency();
+	    }
+
+            time += t;
+            nb++;
+ 
+            for(std::vector<cv::Rect>::const_iterator it = faces.begin(); it != faces.end(); ++it) {
+                cv::rectangle(frame, *it, cv::Scalar(0,0,255));
+            }
+            std::stringstream ss;
+            ss << "FPS=" << (nb / time);
+            cv::putText(frame, ss.str(), cv::Point(30, 30), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0,0,255));
+ 
+            cv::imshow("Frame", frame);
+            char c = cv::waitKey(30);
+            if(c == 27) {
+                break;
+            }
+        }
+    }
+ 
+    std::cout << "Mean time=" << (time / nb) << " s" << " ; Mean FPS=" << (nb / time) << " ; nb=" << nb << std::endl;
+    system("pause");
+    return 0;
+}
